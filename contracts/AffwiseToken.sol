4d3ea8306cc31c04117073b51d57bac671b1487b
Janis Komuls <janis.komuls@gmail.com>
2018-03-15 10:50:59 +0200

    intial affwise token via zeppelin with circuit breaker
    http://truffleframework.com/tutorials/robust-smart-contracts-with-openzeppelin
    https://github.com/OpenZeppelin/zeppelin-solidity

---
+++ contracts/AffwiseToken.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+
+contract AffwiseToken is StandardToken, Pausable {
+
+    string public constant name = "Affwise Token";
+    string public constant symbol = "AFW";
+    uint32 public constant decimals = 18;
+
+    function AffwiseToken(uint _totalSupply) {
+        require(_totalSupply > 0);
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply
+    }
+
+    function transfer(address _to, uint _value) whenNotPaused returns (bool) {
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint _value) whenNotPaused returns (bool) {
+        return super.transferFrom(_from, _to, _value);
+    }
+}
