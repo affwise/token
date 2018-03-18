abe87d6c1f35da9db1b4045befce9c4ffbb2e2f8
Janis Komuls <janis.komuls@gmail.com>
2018-03-18 17:07:25 +0200

    make token ownable

--- contracts/AffwiseToken.sol
+++ contracts/AffwiseToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
 
-contract AffwiseToken is StandardToken, Pausable {
+contract AffwiseToken is StandardToken, Pausable, Ownable {
 
     string public constant name = "Affwise Token";
     string public constant symbol = "AFW";
