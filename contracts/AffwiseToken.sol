905f2dfe6982ad7d903a1b5f1febff5c0aabb5ec
janiskomuls <janis.komuls@gmail.com>
2018-03-22 16:59:27 +0200

    #1 affwise token using truffle/zeppelin - initial

--- contracts/AffwiseToken.sol
+++ contracts/AffwiseToken.sol
@@ -1,31 +1,31 @@
 pragma solidity ^0.4.11;
 
-import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract AffwiseToken is StandardToken, Pausable {
 
-    string public constant name = "Affwise Token";
-    string public constant symbol = "AFW";
-    uint32 public constant decimals = 18;
+  string public constant name = "Affwise Token";
+  string public constant symbol = "AFW";
+  uint32 public constant decimals = 18;
 
-    function AffwiseToken(uint _totalSupply) {
-        require(_totalSupply > 0);
-        totalSupply = _totalSupply;
-        balances[msg.sender] = _totalSupply
-    }
-    
-    function transfer(address _to, uint256 _value) public whenNotPaused validDestination(_to) returns (bool) {
-        return super.transfer(_to, _value);
-    }
+  function AffwiseToken(uint _totalSupply) public {
+    require(_totalSupply > 0);
+    totalSupply_ = _totalSupply;
+    balances[msg.sender] = _totalSupply;
+  }
 
-    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused validDestination(_to) returns (bool) {
-        return super.transferFrom(_from, _to, _value);
-    }
+  function transfer(address _to, uint256 _value) public whenNotPaused validDestination(_to) returns (bool) {
+    return super.transfer(_to, _value);
+  }
 
-    modifier validDestination(address _to) {
-        require(_to != address(0x0));
-        require(_to != address(this));
-        _;
-    }
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused validDestination(_to) returns (bool) {
+    return super.transferFrom(_from, _to, _value);
+  }
+
+  modifier validDestination(address _to) {
+    require(_to != address(0x0));
+    require(_to != address(this));
+    _;
+  }
 }
